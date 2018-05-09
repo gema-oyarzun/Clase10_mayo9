59cb076800c6c1f7073a16ec212b0be3afec6d8a
Gema Oyarzun <37144202+gema-oyarzun@users.noreply.github.com>
2018-05-09 18:43:25 -0300

    Add files via upload

--- page.php
+++ page.php
@@ -9,28 +9,29 @@ $csv = array_map('str_getcsv', file('https://raw.githubusercontent.com/gema-oyar
 ?>
 
 <main role="main" class="container">
-<h1 class="mb-4"><?php print $title;?></h1>
+<h1 class="mb-3"><?php print $title;?>,</h1>
 <div class="row">
 
 <?php for($t = 0; $t < count($csv); $t++){?>
     <div class="col-sm-4 col-md-3 py-3">
-    <h3 class="border-top pt-3"><?php print($csv[$t]['peliculas'])?></h3>
+    <h5 class="border-top pt-3"><?php print($csv[$t]['peliculas'])?></h3>
     
-    <figure style="height:120px; overflow:hidden;">
+    <figure style="height:120px; overflow:hidden; filter: grayscale(80%);">
     
-    <img src="
+    <img src=
     <?php if ($csv[$t]['img'] == NULL){
         print ("img/gris.png");
     } else {
         print ($csv[$t]['img']);
-    };?>" 
+    };?> 
 
     class="img-fluid">
     </figure>
 
-    <p>Año de lanzamiento: <?php print($csv[$t]['año'])?></p>
-    <p>Género: <?php print($csv[$t]['genero'])?></p>
-    <p>Tiempo de duración: <?php print($csv[$t]['duracion'])?> min</p>
+    <p>Año de lanzamiento: <b><?php print($csv[$t]['año'])?></b></p>
+    <p>Género: <b><?php print($csv[$t]['genero'])?></b></p>
+    <p>Tiempo de duración: <b><?php print($csv[$t]['duracion'])?> min</b></p>
+    <p>Español: <b><?php print($csv[$t]['español'])?></b></p>
     </div>
 <?php };?>
 </div>
